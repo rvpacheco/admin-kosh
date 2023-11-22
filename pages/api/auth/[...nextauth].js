f3b3fb40f0accb9e017df9b44031e598e8cf0ac7
rvpacheco <rvpacheco17@gmail.com>
2023-11-22 13:12:34 -0500

    posible fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,44 +1,40 @@
 import clientPromise from '@/lib/mongodb'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
-import NextAuth, { getServerSession } from 'next-auth'
+import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-
-const adminEmails = ['rvpacheco17@gmail.com','koshcolombia@gmail.com'];
+const adminEmails = ['rvpacheco17@gmail.com', 'koshcolombia@gmail.com'];
 
 export const authOptions = {
-  
-    providers: [
-  
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET
-      }),
-    ],
-    adapter: MongoDBAdapter(clientPromise),
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-      session:({session,token,user}) => {
-        if (adminEmails.includes(session?.user?.email)){
-          return session;
-        }else{
-          return false;
-        }
-        
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    session: async ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
       }
+      return null; // En lugar de false, para seguir las convenciones de NextAuth
     }
-    
-  
+  }
 };
 
 export default NextAuth(authOptions);
 
-export async function isAdminRequest(req,res){
-  const session = await getServerSession(req,res,authOptions);
-  if (!adminEmails.includes(session?.user?.email)){
-    res.status(401);
-    res.end();
-    throw 'not admin';
+export async function isAdminRequest(req, res, next) {
+  try {
+    const session = await getServerSession(req, res, authOptions);
+    if (!adminEmails.includes(session?.user?.email)) {
+      return res.status(401).json({ message: 'Not authorized' });
+    }
+    next(); // Continuar con la siguiente middleware si es admin
+  } catch (error) {
+    // Manejar los errores adecuadamente
+    return res.status(500).json({ message: 'Internal server error' });
   }
-
 }
